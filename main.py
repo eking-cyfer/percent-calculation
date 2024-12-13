d26aa7c2c9c33cc97f270856a08efd368792cb04
eking-cyfer <edwardafatsawo20@gmail.com>
2024-12-13 20:01:05 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Storing Marks for Subjects
+subject1 = 85  # Marks for Subject 1
+subject2 = 90  # Marks for Subject 2
+subject3 = 88  # Marks for Subject 3
+subject4 = 88  # Marks for Subject 4
+subject5 = 92  # Marks for Subject 5
+
+# Finding the total marks
+total_marks = subject1 + subject2 + subject3 + subject4 + subject5
+print("The total marks for all subjects is:", total_marks)
+
+# Calculating the percentage (assuming total marks for each subject is 100)
+percentage = (total_marks / 500) * 100
+print("The percentage is:", percentage, "%")
